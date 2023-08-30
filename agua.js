49caa55ff63acf2ce088916b11c1a37530186042
Jonhy Santos <Jonhy1905@hotmail.com>
2023-08-29 21:28:38 -0300

    Erros corrigidos

--- agua.js
+++ agua.js
@@ -1,8 +1,7 @@
-let anos = window.document.querySelector('input#yers')
-let kilos = window.document.querySelector('input#peso')
-let inp = window.document.querySelector('.inp')
-
 function clicar() {
+    let anos = window.document.querySelector('input#yers')
+    let kilos = window.document.querySelector('input#peso')
+    let inp = window.document.querySelector('.inp')
     let idade = Number(anos.value)
     let kg = Number(kilos.value)
     let ml = 40
@@ -12,12 +11,16 @@ function clicar() {
     let q = ml * kg
     let res = window.document.querySelector('p#res')
     let form = window.document.querySelector('div#form')
-    if(anos.value == 0){
+    if(anos.value.length == 0){
+        anos.style.border = '3px solid red'
+        anos.style.background = '#ffff0092'
         window.alert('Por favor digite sua idade')
-    }else if(kilos.value === 0){
+    }else if(kilos.value.length == 0){
         window.alert('Por favor digite seu peso')
+        kilos.style.border = '3px solid red'
+        kilos.style.background = '#ffff0092'
     }else{
-        res.innerHTML = `Você tem deve beber ${q} ml  por dia`
+        res.innerHTML = `Você deve beber ${q} ml  por dia`
         form.style.height = '300px'
         res.style.background = 'blue'
         res.style.color = 'white'
